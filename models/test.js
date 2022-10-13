0021961f0d00852683c03aaf08c131f969bf2a24
RyuRi996 <lypalgcd191295@fpt.edu.vn>
2022-10-13 11:19:22 +0700

    lol

---
+++ models/test.js
@@ -0,0 +1,3 @@
+var authenticator = require("./authenticator");
+authen=authenticator("ly","1");
+console.log(authen);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.ejs', { title: 'Express' });
+  res.render('index.ejs', { title: 'ATN SHOP' });
+});
+router.post('/', function(req, res, next) {
+  res.render('login.ejs', { title: 'ATN SHOP' });
+});
+
+router.post('/login', function(req, res, next) {
+  
+  res.render('users.ejs', { title: 'WELCOME BACK' });
 });
 
 module.exports = router;
